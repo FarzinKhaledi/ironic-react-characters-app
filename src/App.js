037409f93dc637402789603245341a91a1fb873b
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-10-21 11:34:12 +0200

    add functionality to delete character

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import axios from 'axios';
-import {Routes, Route, Link} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import './App.css';
 import CharacterDetails from './components/CharacterDetails';
 import CharactersList from './components/CharactersList';
+
 
 function App() {
 
   const [charactersArr, setCharactersArr] = useState([]);
 
   useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = () => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
-      .then( response => {
+      .then(response => {
         setCharactersArr(response.data);
       })
       .catch(e => console.log("error getting characters from API", e));
-  }, []);
+  }
 
 
   return (
     <div className="App">
-
-        <Routes>
-          <Route path='/' element={<CharactersList characters={charactersArr} />} />
-          <Route path='/characters/:characterId' element={<CharacterDetails />} />
-        </Routes>
-
-
-
+      <Routes>
+        <Route path='/' element={<CharactersList characters={charactersArr} />} />
+        <Route path='/characters/:characterId' element={<CharacterDetails callbackToFetchCharacters={fetchCharacters} />} />
+      </Routes>
     </div>
   );
 
